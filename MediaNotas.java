f8afa82d96ee7cb53f35a044929acf158438e0cd
dev.yago <yagoromualdo.yr@gmail.com>
2024-10-28 22:53:32 -0300

    Fase Green

---
+++ MediaNotas.java
@@ -0,0 +1,8 @@
+package fase_2_green;
+
+public class MediaNotas {
+
+    public static double calculaMedia(double nota1, double nota2, double nota3) {
+        return (nota1 + nota2 + nota3) / 3;
+    }
+}
---
+++ MediaNotasTest.java
@@ -0,0 +1,27 @@
+package fase_2_green;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MediaNotasTest {
+
+    @Test
+    public void testMediaTresNotas() {
+        assertEquals(5.0, MediaNotas.calculaMedia(5.0, 5.0, 5.0), 0.001);
+    }
+
+    @Test
+    public void testTodasNotasZero() {
+        assertEquals(0.0, MediaNotas.calculaMedia(0.0, 0.0, 0.0), 0.001);
+    }
+
+    @Test
+    public void testNotasMaximas() {
+        assertEquals(10.0, MediaNotas.calculaMedia(10.0, 10.0, 10.0), 0.001);
+    }
+
+    @Test
+    public void testNotasComDecimais() {
+        assertEquals(6.5, MediaNotas.calculaMedia(5.5, 6.5, 7.5), 0.001);
+    }
+}
